14b82e79c6aff1ae6926b4b9dddc83310b09d58e
Franz-Zhao <51204108042@stu.ecnu.edu.cn>
2021-10-26 21:40:37 +0800

    modify

--- app/config.js
+++
@@ -1,7 +0,0 @@
-// 通常写在环境变量中!
-module.exports = {
-    // MongoDB链接
-    connectionStr: 'mongodb+srv://franz:Fz020162540@franzmongodb.iqdtm.mongodb.net/FranzMongodb?retryWrites=true&w=majority',
-    // token签名的密码
-    secret: 'franz-jwt-secret',
-};
